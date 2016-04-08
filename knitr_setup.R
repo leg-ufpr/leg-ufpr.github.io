85e178c8931afd4393be58775edbf57e869a199e
Fernando Mayer <fernandomayer@gmail.com>
2016-04-07 21:19:10 -0300

    First version created

---
+++ knitr_setup.R
@@ -0,0 +1,14 @@
+library(knitr)
+opts_chunk$set(
+    ## knitr options
+    cache = TRUE,
+    tidy = FALSE,
+    comment = NA,
+    fig.width = 10,
+    fig.height = 8,
+    fig.align = "center",
+    dpi = 80, ## aumentar o dpi para exibir maior.
+    dev = "png",
+    fig.path = "figures/",
+    cache.path = "cache/"
+    )
